25ae8d88e66bc5f499185a3e68403726fea2e0bf
unknown <p229270@pwr.nu.edu.pk>
2025-09-30 01:52:43 +0500

    Take about string to nan conversion

--- variableConfusion.js
+++ variableConfusion.js
@@ -1,7 +1,14 @@
-let score = "33"
+let score = "33" // String but can be converted to number
+let score2 = "44abc"  // String but cannot be converted to number 
+
 
 console.log(typeof score)
 
 let ValueInNumer = Number(score)
 
 console.log(typeof ValueInNumer)
+
+let ValueInNumer2 = Number(score2)
+
+console.log(ValueInNumer2)  // NaN (Not a Number) but still javascript considers it as number type
+console.log(typeof ValueInNumer2)  // number
