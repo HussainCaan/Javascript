1390757e68a1adfad6beb8ca250f91b1f69f7033
unknown <p229270@pwr.nu.edu.pk>
2025-10-18 19:13:13 +0500

    nums and maths functions

--- nums & maths.js
+++ nums & maths.js
@@ -21,14 +21,21 @@ console.log(Math.ceil(4.2)) // 5
 console.log(Math.floor(4.7)) // 4
 console.log(Math.round(4.5)) // 5
 console.log(Math.round(4.4)) // 4
-console.log(Math.max(1,4,2,8,3)) // 8
-console.log(Math.min(1,4,2,8,3)) // 1
-console.log(Math.pow(2,3)) // 8
+console.log(Math.max(1, 4, 2, 8, 3)) // 8
+console.log(Math.min(1, 4, 2, 8, 3)) // 1
+console.log(Math.pow(2, 3)) // 8
 console.log(Math.sqrt(64)) // 8
 console.log(Math.random().toFixed(2)) // random number between 0 and 1
 console.log((Math.random().toFixed(2) * 100)) // random number between 0 and 100
-console.log(Math.floor(Math.random() * 100) + 1 ) // random number between 1 and 100
+console.log(Math.floor(Math.random() * 100) + 1) // random number between 1 and 100
 console.log(Math.sin(0)) // 0
 console.log(Math.cos(0)) // 1
 console.log(Math.log(1)) // 0
 console.log(Math.log10(100)) // 2 // log base 10 of 100 is 2
+
+let min = 10
+let max = 50
+console.log(Math.floor(Math.random() * (max - min + 1) + min )) // random number between min and max (both inclusive)
+
+// Remember that Math object methods are static methods and we cannot create instance of Math object
+// let mathObj = new Math()  // this will give error
