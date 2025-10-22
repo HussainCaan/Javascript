0e58f610c2ab61d57557cff6c9cf473b41c5312c
unknown <p229270@pwr.nu.edu.pk>
2025-10-22 23:46:54 +0500

    functions basics

---
+++ functions/functions.js
@@ -0,0 +1,9 @@
+function addtwonumbers(number1, number2){
+    return Number(number1) + Number(number2);
+}
+// console.log(addtwonumbers(1, 2));
+// console.log(addtwonumbers("1", 4)); // as 1 is string it takes 4 also as string
+// // but what if
+// console.log(addtwonumbers("a", 4)); // As a is given we will give 
+let result = addtwonumbers(3,5);
+console.log(result)
