baedd0c44be2ac51eb6d76d51ccb6c68a9c26e24
unknown <p229270@pwr.nu.edu.pk>
2025-10-27 00:03:19 +0500

    Javascript execution

---
+++ Execution context/Execution.js
@@ -0,0 +1,7 @@
+// There are two type of execution context in js
+// 1: Global EC // Allocate into this in node environment and in browser there is window object for it
+// 2: Function EC
+// When js runs
+// First is memory creation phase MCP (In this phase create memory for all variables and store it don't execute it)
+// Second is Execution Phase 
+// 
