97da2c9513aad7396732da46692e62ad70c3fa58
unknown <p229270@pwr.nu.edu.pk>
2025-10-27 01:14:31 +0500

    for loop

---
+++ loops/for_Iterations.js
@@ -0,0 +1,46 @@
+// for loop
+// for(let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+// Table of 3 ----------------------------------------------------------------------------------------
+// const array = [1,2,3,4,5,6,7,8,9,10];
+// for(let index = 0; index < array.length; index++){
+//     console.log(3 * array[index]);
+// }
+
+// loop with if else and continue & break keywords
+
+// for(let i = 1; i < 10; i++){
+//     if(i%2 == 0){
+//         console.log(`${i} is an even number`);
+//     }
+//     else{
+//         console.log(`${i} is an odd number`);
+//     }
+// }
+
+
+// for(let i = 1; i < 10; i++){
+//     if(i%2 == 0){
+//         console.log(`${i} is an even number`);
+//         if(i == 8){
+//             break; // it break on 8
+//         }
+//         else{
+//             continue;
+//         }
+//     }
+//     else{
+//         console.log(`${i} is an odd number`);
+
+//     }
+// }
+
+
+// let array = [1, 2, 3], array2 = [7, 8, 9];
+// let mul = 0;
+// for (let i = 0; i < array.length; i++) {
+//     for (let j = 0; j < array2.length; j++) {
+//         mul += array[i] * array2[j]
+//     }
+// }
