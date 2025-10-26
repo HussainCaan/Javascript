7dc40fe1449ca2d7f92ef5029be29e41154d0c4d
unknown <p229270@pwr.nu.edu.pk>
2025-10-27 00:47:32 +0500

    truthy and falsy values

---
+++ Control_Flow/Truthy.js
@@ -0,0 +1,30 @@
+// const userEmail = "Hussain@google.com";
+const userEmail = "";
+
+if (userEmail){
+    console.log("Got user email");
+}
+else{
+    console.log("Did not get the user email");
+}
+
+
+// Falsy Values
+// [false, 0, -0, in BigInt 0n, "", null, undefined, NaN]
+
+// Truthy Values
+// [true, 1, "0", 'false', " ", [], {}, function(){}, ]
+
+
+// const array = [];
+// if (array.length === 0) {
+//     console.log('array is empty');
+// }
+
+// const object = {}
+
+// if (Object.keys(object).length === 0) {
+//     console.log('Object is empty')
+// }
+
+
