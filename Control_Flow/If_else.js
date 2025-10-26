154dfbbeceb02aee6f0ebb2bacdb52d6a71fe664
unknown <p229270@pwr.nu.edu.pk>
2025-10-27 00:31:32 +0500

    Control flow

---
+++ Control_Flow/If_else.js
@@ -0,0 +1,24 @@
+// let month = 3;
+
+// if (month == 3) {
+//     console.log("It's March");
+// }
+// else{
+//     console.log("It's not March");
+// }
+
+// if(true){
+//     let power = "Fly";
+//     console.log(`User power is ${power}`);
+// }
+
+
+// const isLoggedin = true, Debitcard = true, LoggedInFromGoogle = true, LoggedInFromEmail = false;
+
+// if ((isLoggedin && Debitcard) && (LoggedInFromGoogle || LoggedInFromEmail) ){
+//     console.log("You can buy anything");
+// }
+// else{
+//     console.log("Please make sure you have entered debit card and also you are logged in");
+// }
+
