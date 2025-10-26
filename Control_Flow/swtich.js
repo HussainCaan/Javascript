f9a39437f2b606c9f2348c7bf20e80355ec85909
unknown <p229270@pwr.nu.edu.pk>
2025-10-27 00:36:32 +0500

    switch cases

---
+++ Control_Flow/swtich.js
@@ -0,0 +1,11 @@
+let name = "Hussain";
+
+switch (name){
+    case "Hussain":
+        console.log("Hussain is logged in");
+        break;
+    case "Hassan":
+        console.log("Hassan is logged in");
+    default :
+        console.log("No one is loggedIn");
+}
