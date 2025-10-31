6c253bda6e9c49dcaa1157aba323da4c6547272f
unknown <p229270@pwr.nu.edu.pk>
2025-10-31 21:20:05 +0500

    whole and do whole loops

---
+++ loops/while&dowhile.js
@@ -0,0 +1,14 @@
+// let  i = 1
+// while(i <= 5){ // first check condition then run the content inside the scope loop
+//     console.log(i);
+//     i++
+// }
+
+let i = 1
+do { // first run the content inside loop scope then check condition
+    console.log(i);
+    i++
+} while (i <=5);
+
+
+
