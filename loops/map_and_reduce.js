41c7d86c745760b9520f0d92e34f5da7e17e8078
unknown <p229270@pwr.nu.edu.pk>
2025-11-12 01:43:42 +0500

    maps

---
+++ loops/map_and_reduce.js
@@ -0,0 +1,6 @@
+// .map function is same as filter but it can return values by default like for example
+
+let mynums = [1,2,3,4,5,6,7,8,9,10];
+
+let newnums = mynums.map((num)=> num+10); // adding 10 to each number of array
+console.log(newnums)
